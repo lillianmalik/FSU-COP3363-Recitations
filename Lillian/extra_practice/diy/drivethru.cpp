4f73f002738005c4808033df8ed5da282b37266a
Lillian <lillianmalik674@gmail.com>
2026-09-25 10:43:45 -0400

    Practice Coding Problem
    Something fun for students to apply the new things learned in lecture & recitation

---
+++ Lillian/extra_practice/diy/drivethru.cpp
@@ -0,0 +1,89 @@
+#include <iostream>     // for cin/cout
+#include <string>       // for no pain and suffering (char arrays)
+#include <iomanip>      // for decimal places (cents)
+using namespace std;
+
+/*
+    DISCLAIMER:
+    THIS IS NOT A GRADED ASSIGNMENT, YOU DO NOT NEED TO SUBMIT THIS ONLINE
+    OR TO YOUR TA.
+
+    IT IS PURELY FOR YOU TO PRACTICE WITH! HAVE FUN WITH IT!
+*/
+
+int main() {
+
+/*
+
+    THE MCDONALD'S DRIVER THRU SIMULATOR
+    ====================================
+    You will need:
+        - cin/cout statements
+        - setprecision()
+        - valid input checkpoints (if-else)
+        - a loop for the user to add multiple items
+        - a loop to continually display the menu
+        - your \t \n \" funny characters for your output
+
+    Menu:
+        1. McChicken ($1.00)
+        2. Big Mac ($5.00)
+        3. French Fries
+            - Small ($2.00)
+            - Medium ($2.50)
+            - Large ($3.00)
+        4. Chicken nuggets
+            - 4ct ($2.00)
+            - 8ct ($4.00)
+            - 16ct ($8.00)
+        5. Soda
+            - Small ($1.50)
+            - Medium ($2.00)
+            - Large ($2.50)
+        6. McFlurry
+            - Small ($2.50)
+            - Medium ($3.00)
+            - Large ($3.50)
+
+    The Output:
+        + The Menu
+        - "Hi welcome to McDonald's what can I get started for you?"
+        < user enters a number >
+
+        - "Will that be all for you today?"
+        < y/n >
+
+        + The Menu
+        - "What else?"
+        < user enters a number >
+
+        - * continues to ask if that will be all until the user answers "no" *
+
+        - "Alright, your total is going to be $x.xx at the first window."
+        -----------------------------------
+        RECEIPT:
+            - item 1        ($x.xx)
+            ...
+            - item i        ($price)
+            TOTAL: $x.xx
+        -----------------------------------
+
+    Optional Extra features:
+        - add taxes (0.07 * total)      ** I really encourage you to try this one
+        - "make it a meal" functionality
+        - round up to donate to charity
+        - pay for the guy behind you (randomly generate their meal) (we haven't learned this yet)
+        - the icecream machine is broken
+
+    NOTES:
+        - You don't have to make this simulator a 1:1 copy of what
+        you think I want to see lol.
+        - This is purely for YOUR practice, IT IS NOT GRADED
+        - Have fun with it! Add stupid stuff, break it and then fix it again!
+        I hope this is helpful for you guys :)
+    
+*/
+    
+
+    return 0;
+}
